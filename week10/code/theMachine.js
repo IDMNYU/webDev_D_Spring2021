7326d386ebb2a913f920470d6d67cd4656665a52
rebleo <rml444@nyu.edu>
2021-04-08 15:19:38 -0400

    jq

--- week10/code/theMachine.js
+++ week10/code/theMachine.js
@@ -1,3 +1,23 @@
 console.log("the machine!");
 
+let theP = "yr a hero"
 
+var dat = $('#myButton')
+dat.on('click', potato);
+
+function potato() {
+    console.log("potato click!");
+    $('h3').append(theP)
+    $('h4').append("<p>"+ theP + "</p>");
+  
+}
+
+var here = $('#clickHere')
+here.click(function() {
+    console.log("yellow click");
+    $('#other').text("now the green text is this");
+    $('div').css("background-color", "red")
+    $('body').css("background-color", "blue");
+    $('h3').remove(":contains('hello')");
+
+})
